731f430862a92681e23fc791e229983fa2cf1db1
Natasza ASzatan <natiye.nb@gmai.com>
2024-07-17 12:17:09 +0100

    added function add new cluster

--- main.js
+++ main.js
@@ -1,73 +1,86 @@
 import "./style.css";
 
 const clusters = [
-    [3, 5, 10, 11],
-    [55, 60, 62]
-]
+  [3, 5, 10, 11],
+  [55, 60, 62],
+];
 const clustersMapping = {
-    3: clusters[0],
-    5: clusters[0],
-    10: clusters[0],
-    11: clusters[0],
-    55: clusters[1],
-    60: clusters[1],
-    62: clusters[1],
-}
+  3: clusters[0],
+  5: clusters[0],
+  10: clusters[0],
+  11: clusters[0],
+  55: clusters[1],
+  60: clusters[1],
+  62: clusters[1],
+};
 
 // add element with value 13
 const absolutDistance = 5;
 const valueToAdd = 13;
 
 function findClusterForValue(value) {
-    let foundCluster = null
-    let searchValue = value - absolutDistance;
+  let foundCluster = null;
+  let searchValue = value - absolutDistance;
 
-    while(!foundCluster) {
-        if(clustersMapping[searchValue]) {
-            foundCluster = clustersMapping[searchValue];
-        } else {
-            searchValue++;
-        }
-        if(searchValue > value + 5) {
-            break;
-        }
+  while (!foundCluster) {
+    if (clustersMapping[searchValue]) {
+      foundCluster = clustersMapping[searchValue];
+    } else {
+      searchValue++;
     }
-    return foundCluster;
+    if (searchValue > value + 5) {
+      break;
+    }
+  }
+  return foundCluster;
 }
 
 function addToCluster(value) {
-    const foundCluster = findClusterForValue(value);
+  const foundCluster = findClusterForValue(value);
 
-    if (foundCluster) {
-        if(foundCluster.includes(value)){
-            return "value already in cluster"
-        }
-        foundCluster.push(value);
-        clustersMapping[value] = foundCluster;
-        rebuildCluster(foundCluster)
+  if (foundCluster) {
+    if (foundCluster.includes(value)) {
+      return "value already in cluster";
     }
+    foundCluster.push(value);
+    clustersMapping[value] = foundCluster;
+    rebuildCluster(foundCluster);
+  } else {
+    addNewCluster(value);
+    console.log(clustersMapping);
+  }
 
-    return foundCluster;
+  return foundCluster;
 }
 
 function removeFromCluster(value) {
-    const foundCluster = findClusterForValue(value);
+  const foundCluster = findClusterForValue(value);
 
-    if (foundCluster) {
-        const indexOfValueInCluster = foundCluster.indexOf(value)
-        foundCluster.splice(indexOfValueInCluster,1)
-        delete clustersMapping[value]
-    } else {
-        return "no such value in clusters"
-    }
-
-    return foundCluster;
+  if (foundCluster) {
+    const indexOfValueInCluster = foundCluster.indexOf(value);
+    foundCluster.splice(indexOfValueInCluster, 1);
+    delete clustersMapping[value];
+  } else {
+    return "no such value in clusters";
+  }
+  return foundCluster;
 }
 
-function rebuildCluster(cluster){
-    cluster.sort(function(a,b){return a-b})
+function rebuildCluster(cluster) {
+  cluster.sort(function (a, b) {
+    return a - b;
+  });
 }
 
-addToCluster(valueToAdd)
+function addNewCluster(value) {
+  const newCluster = [value];
+  clusters.push(newCluster);
+  const indexOfCluster = clusters.indexOf(newCluster);
+  clustersMapping[value] = clusters[indexOfCluster];
+}
 
-
+addToCluster(valueToAdd);
+addToCluster(14);
+addToCluster(82);
+addToCluster(84);
+removeFromCluster(82);
